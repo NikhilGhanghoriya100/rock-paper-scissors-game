c89652ce58de6446b2ef22d07aa11fee56906a60
NikhilGhanghoriya100 <nikhilghanghoriya2@gmail.com>
2025-10-23 10:56:06 +0530

    new changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let userScore = 0;
 let compScore = 0;
 
-const choices = document.querySelectorAll(".choice");
+const choices = document.querySelectorAll(".choice"); 
 const msg = document.querySelector("#msg");
 const userScorePara = document.querySelector("#user-score");
 const compScorePara = document.querySelector("#comp-score");
